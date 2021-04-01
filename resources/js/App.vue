d49a7865599c6cec3d7a0eb5317e6728e15bbdc9
Daniël van Drunen <daniel@pixely.me>
2021-04-01 20:20:13 +0200

    Implement drag and drop functionality for appointments

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,10 +2,10 @@
     <div class="wrapper">
         <div v-for="(day, i) in days" :key="'day-' + i" class="day">
             <div class="day-label">{{ day }}</div>
-            <div v-for="(hour, j) in endingHour - startingHour" :key="'hour-' + j" class="hour">
+            <div v-for="(hour, j) in endingHour - startingHour" :key="'hour-' + j" class="hour" :data-datetime="day + ' ' + ((startingHour + hour - 1).toString().padStart(2, 0)) + ':00:00'">
                 <div class="hour-label">{{ (startingHour + hour - 1).toString().padStart(2, 0) }}:00</div>
                 <div class="appointments">
-                    <div v-for="appointment in getAppointments(day, startingHour + hour - 1)" :key="'appointment-' + appointment.id" class="appointment">
+                    <div v-for="appointment in getAppointments(day, startingHour + hour - 1)" :key="'appointment-' + appointment.id" class="appointment" draggable="true" :data-id="appointment.id">
                         {{ appointment.memo }}
                     </div>
                 </div>
@@ -22,7 +22,8 @@ export default {
             startingHour: 9,
             endingHour: 17,
 
-            appointments: []
+            appointments: [],
+            draggingPayload: null
         };
     },
 
@@ -63,6 +64,61 @@ export default {
          */
 
         this.fetchAppointments();
+    },
+
+    updated() {
+        /**
+         * Appointments (payloads)
+         */
+
+        document.querySelectorAll('.appointment').forEach(element => {
+            element.addEventListener('dragstart', e => {
+                this.draggingPayload = e.target;
+            });
+        });
+
+        /**
+         * Hours (targets)
+         */
+
+        document.querySelectorAll('.hour').forEach(element => {
+            /**
+             * I have no idea why, but the "drop" event only triggers if the
+             * following 2 events have been prevented..
+             */
+
+            element.addEventListener('dragenter', e => {
+                e.preventDefault();
+            });
+
+            element.addEventListener('dragover', e => {
+                e.preventDefault();
+            });
+
+            /**
+             * And finally, the actual "drop" event
+             */
+
+            element.addEventListener('drop', e => {
+                // Detach from previous parent
+                this.draggingPayload.parentNode.removeChild(this.draggingPayload);
+
+                element.querySelector('.appointments').appendChild(this.draggingPayload);
+
+                /**
+                 * Notify API of drag
+                 */
+
+                const id = this.draggingPayload.dataset.id;
+                const targetDateTime = element.dataset.datetime;
+
+                axios.post('/api/appointments/' + id + '/update', {
+                    scheduled_at: targetDateTime
+                }).then(() => {
+                    console.log('gg');
+                });
+            });
+        });
     }
 };
 </script>
@@ -109,5 +165,9 @@ export default {
     background: #FFF;
     border: 1px solid #CCC;
     border-radius: 8px;
+}
+
+.appointment:hover {
+    cursor: pointer;
 }
 </style>
