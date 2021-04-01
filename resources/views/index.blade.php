683c63c7745cd2dfab13982a180f568cc35a50eb
Daniël van Drunen <daniel@pixely.me>
2021-04-01 19:13:45 +0200

    Set up Vue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,10 @@
-Hello world
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{{ config('app.name') }}</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
